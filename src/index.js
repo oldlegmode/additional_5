5f8acfe6fb59081b5bdd4df37ccd7ede23840e43
Oleg <crashyourselfcontrol@gmail.com>
2018-02-26 00:58:23 +0300

    config doesnt work, but more then 50% tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check;
+
+function check(str, bracketsConfig) {
+  let objEnableBrackets = {};
+  let objConfigBrackets = {};
+
+  for (let char of str) {
+    objEnableBrackets[char] = (objEnableBrackets[char] || 0) + 1;
+  }
+  for (let key in objEnableBrackets) {
+    if (key === '(') {
+      if (objEnableBrackets[key] !== objEnableBrackets[')']) return false;
+    }
+    if (key === '[') {
+      if (objEnableBrackets[key] !== objEnableBrackets[']']) return false;
+    }
+    if (key === '{') {
+      if (objEnableBrackets[key] !== objEnableBrackets['}']) return false;
+    }
+
+  }
+  return true;
 }
